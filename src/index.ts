be7af937d5eabd394773ac93602b7183695eddc9
Lawen <lawen.thaya94@gmail.com>
2025-12-11 17:10:14 +1100

    slimmed everything down. added exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 
-type DirArgs = [number, number];
+type DirArgs = [number, number, string];
 type PlaceArgs = [number, number, DirArgs];
 type DirectionField = Record<string, DirArgs>;
 type CommandsType = Record<string, number>;
 
 
 const DirectionMap: DirectionField = {
-  NORTH: [0, 1],
-  WEST: [-1, 0],
-  SOUTH: [0, -1],
-  EAST: [1, 0],
+  NORTH: [0, 1, "⏫"],
+  WEST: [-1, 0, "⏪"],
+  SOUTH: [0, -1, "⏬"],
+  EAST: [1, 0, "⏩"],
 }
 
 const ValidCommands: CommandsType = {
@@ -23,6 +23,15 @@ const ValidCommands: CommandsType = {
   EXIT: 5,
 }
 
+//const CommandMap = {
+//  PLACE: 0,
+//  MOVE: 1,
+//  REPORT: 2,
+//  LEFT: 3,
+//  RIGHT: 4,
+//  EXIT: 5,
+//}
+
 
 class Map {
   width: number;
@@ -31,33 +40,35 @@ class Map {
   constructor(x = 5, y = 5) {
     this.width = x;
     this.height = y;
-
-    console.log(``);
-    console.log(`New Map`);
-    console.log(`Map size: width: ${this.width} height: ${this.height}`);
+    console.log(`[Map][constructor] width: ${this.width} height: ${this.height}`);
   }
 
-  //validate_placement() {}
+  is_within_map(x = 0, y = 0) {
+    return (
+      (x >= 0) && (x < this.width) &&
+      (y >= 0) && (y < this.height)
+    )
+  }
 }
 
 
 class Robot {
-  map: Map | undefined;
   pos_x: number;
   pos_y: number;
   direction: DirArgs;
   is_placed: boolean;
 
-  constructor(map: Map | undefined, x = -1, y = -1, direction:DirArgs = [0, 0]) {
+  constructor(x = -1, y = -1, direction:DirArgs = [0, 0, "🟦"]) {
     this.is_placed = false;
-    this.map = map;
     this.pos_x = x;
     this.pos_y = y;
     this.direction = direction;
 
-    console.log(``);
-    console.log(`New Robot`);
-    console.log(`Robot At: pos_x: ${this.pos_x} pos_y: ${this.pos_y} map: width: ${this.map?.width} height: ${this.map?.height}`);
+    console.log(`[Robot][constructor] pos_x: ${this.pos_x} pos_y: ${this.pos_y}`);
+  }
+
+  get_tile(): string {
+    return this.direction[2]
   }
 }
 
@@ -72,11 +83,38 @@ class Game {
   }
 
   update() {
-    console.log(``);
-    console.log(`Robot Position`);
+    console.log(`\nRobot Position`);
     console.log(`pos_x: ${this.robot.pos_x}`);
     console.log(`pos_y: ${this.robot.pos_y}`);
-    console.log(`direction: ${this.robot.direction}`);
+    console.log(`direction: ${this.robot.direction}\n`);
+  }
+}
+
+class Renderer {
+  robot = "🤖";
+  tile = "🟥";
+
+  constructor() {
+    console.log(`[Renderer][constructor] robot: ${this.robot} tile: ${this.tile}`);
+  }
+
+  print_frame(map: Map, robot: Robot): void {
+    console.log("------------");
+    for (let j = 0; j < map.height; j++) {
+      const row: string[] = [];
+      if (j===robot.pos_y) {
+        for (let i = 0; i < map.width; i++) {
+          if (i===robot.pos_x) {
+            row.push(robot.get_tile())
+          } else {
+            row.push(this.tile)
+          }
+        }
+      } else {
+        row.push(...new Array(map.width).fill(this.tile));
+      }
+      console.log(row.join(''));
+    }
   }
 }
 
@@ -85,13 +123,11 @@ class Instruction {
   command: string;
   args: string;
   is_valid: boolean;
-  place_args: PlaceArgs;
 
   constructor(command = "") {
     this.is_valid = true;
     this.command = command;
     this.args = "";
-    this.place_args = [-1, -1, [0, 0]];
   }
 
   set_args(args: string) {
@@ -132,11 +168,6 @@ function process_input(valid_input_arr: string[]): [boolean, Instruction] {
 }
 
 
-//
-//function process_command(valid_command: Instruction): void {
-//  valid_command.is_valid = true;
-//}
-
 
 //
 function parse_argument_place(instruction: Instruction): [boolean, PlaceArgs]  {
@@ -145,18 +176,18 @@ function parse_argument_place(instruction: Instruction): [boolean, PlaceArgs]  {
       const x_pos = Number(strings[0]);
       const y_pos = Number(strings[1]);
       const dir_str = strings[2];
-      const default_dir: DirArgs = [0,0];
+      const default_dir: DirArgs = [0, 0, "🟦"];
       const dir_int = dir_str in DirectionMap ? DirectionMap[dir_str] : default_dir;
       instruction.is_valid = true;
       return [true, [x_pos, y_pos, dir_int]]
   }
   instruction.is_valid = false;
-  return [false, [-1, -1, [0, 0]]]
+  return [false, [-1, -1, [0, 0, "🟦"]]]
 }
 
 
-//function handle_command_place(args: [number, number, DirArgs], game: Game): void {
-function handle_command_place(instruction: Instruction, game: Game): void {
+//function evaluate_instruction_place(args: [number, number, DirArgs], game: Game): void {
+function evaluate_instruction_place(instruction: Instruction, game: Game): void {
   const [is_valid, porcessed_args] = parse_argument_place(instruction);
 
   if (is_valid) {
@@ -174,11 +205,10 @@ function handle_command_place(instruction: Instruction, game: Game): void {
 }
 
 
-function handle_command(instruction: Instruction, game: Game): void {
+function evaluate_instruction(instruction: Instruction, game: Game): void {
   if (instruction.command === "PLACE") {
     // validate placement
-
-    handle_command_place(instruction, game);
+    evaluate_instruction_place(instruction, game);
     return
   }
   else if (game.robot.is_placed) {
@@ -193,61 +223,46 @@ function handle_command(instruction: Instruction, game: Game): void {
           return
       case "EXIT":
           return
-      //default:
-      //    return
     }
   }
 }
 
 
-function handle_input(game: Game, raw_input: string) {
-
+function read_input(raw_input: string): Instruction {
+  // we need to throw errors to be SOLID
   const [is_clean, cleaned_input] = clean_input(raw_input);
 
   if (is_clean) {
 
-    // check if the input is valid, correct length
-    // correct number of elements after splitting
     const [is_valid, validated_input] = validate_input(cleaned_input);
 
     if (is_valid) {
-
-      // if the nput is valid, we then process it
       const [is_instruction, instruction] = process_input(validated_input);
-
-      // valid instruction
       if (is_instruction) {
-
-        // if the robot is placed we process any instruction
-        handle_command(instruction, game);
-
-        //if (game.robot.is_placed) {
-        //}
-        //else if (instruction.command === "PLACE") {
-        //  // validate placement
-        //  handle_command_place(instruction, game);
-        //}
+        return instruction
       }
     }
   }
+  throw new Error("Invalid Input");
 }
 
 
 function main() {
     const map = new Map();
-    const robot = new Robot(map);
+    const robot = new Robot();
+    const renderer = new Renderer();
 
     // create game
     const game = new Game(map, robot);
 
-    handle_input(game, "PLACE 1,2,NORTH");
-    game.update();
-
-    handle_input(game, "PLACE 3,3,SOUTH");
-    game.update();
-
+    try {
+      const instruction= read_input("PLACE 1,2,NORTH");
+      evaluate_instruction(instruction, game);
+      renderer.print_frame(map, robot)
+    } catch (error) {
+      console.error(`Failed: ${error}`);
+    }
     console.log("Done");
 }
-
 
 main();
